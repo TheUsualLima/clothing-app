36fff16aff8248ad766fc9d6a777fba4e730d03d
Navin Hewadewa <theusuallima@gmail.com>
2021-02-08 17:16:07 +0000

    fragments setup

---
+++ app/src/main/java/com/jason/clothingdisplay/ui/mainmenu/MainMenuFragment.kt
@@ -0,0 +1,6 @@
+package com.jason.clothingdisplay.ui.mainmenu
+
+import androidx.fragment.app.Fragment
+import com.jason.clothingdisplay.R
+
+class MainMenuFragment : Fragment(R.layout.activity_main_menu)
---
+++ app/src/main/java/com/jason/clothingdisplay/ui/productslist/ProductsListFragment.kt
@@ -0,0 +1,6 @@
+package com.jason.clothingdisplay.ui.productslist
+
+import androidx.fragment.app.Fragment
+import com.jason.clothingdisplay.R
+
+class ProductsListFragment : Fragment(R.layout.fragment_products_list)
