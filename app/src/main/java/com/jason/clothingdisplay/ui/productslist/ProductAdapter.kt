5891cfa09fc878bb7fb0f11b929842ee5ee89a29
Navin Hewadewa <theusuallima@gmail.com>
2021-02-10 06:04:19 +0000

    product images displayed with glide

--- app/src/main/java/com/jason/clothingdisplay/ui/productslist/ProductAdapter.kt
+++ app/src/main/java/com/jason/clothingdisplay/ui/productslist/ProductAdapter.kt
@@ -3,8 +3,10 @@ package com.jason.clothingdisplay.ui.productslist
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jason.clothingdisplay.R
 import com.jason.clothingdisplay.databinding.ProductsListItemBinding
 import com.jason.clothingdisplay.domain.network.Product
@@ -24,6 +26,16 @@ class ProductsAdapter() : ListAdapter<Product, ProductsAdapter.ProductsViewHolde
             itemBinding.apply {
                 productName.text = currentItem.name
                 productPrice.text = context.resources.getString(R.string.price, String.format("%.2f", currentItem.price))
+                val url = "https://riverisland.scene7.com/is/image/RiverIsland/${currentItem.id}_main"
+                Glide.with(context)
+                    .load(url)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_foreground)
+                    .into(productImage)
+                productItemContainer.setOnClickListener {
+                    Toast.makeText(context, "${currentItem.name} clicked", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
